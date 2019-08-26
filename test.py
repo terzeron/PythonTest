d273324cc1cf7eae34ba55dc05989f65f2efc6fc
Youngil Cho <terzeron@gmail.com>
2019-08-27 08:01:31 +0900

    add a test application add python version file

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import  threading
+
+def worker(data):
+    try:
+        print(data.value)
+        data.value += 1
+        print(data.value)
+    except AttributeError:
+        print("no value")
+
+
+local_data = threading.local()
+local_data.value = 10
+
+for i in range(10):
+    t = threading.Thread(target=worker, args=(local_data,))
+    t.start()
+    
