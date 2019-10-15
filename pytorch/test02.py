574fd44fa4ab455dbc26bdb23e7784e3d31372f7
Young-il Cho <youngilcho@nhnent.com>
2019-10-15 16:59:23 +0900

    add comments

--- pytorch/test02.py
+++ pytorch/test02.py
@@ -4,13 +4,18 @@
 
 import torch
 from torch.autograd import Variable
+# autograd는 자동 미분
+# 실행-기반-정의 (define by run)
+# 역전파 기능이 제공됨
+# autograd.Variable로 tensor를 감싸서 연산을 할 수 있음
+# variable에 대해 backward()를 호출하여 자동으로 변화도(gradient)를 계산해냄(역전파)
 
 x = Variable(torch.ones(2, 2), requires_grad=True)
 print("x=", x)
 
 y = x + 2
 print("y=x+2=", y)
-print(y.grad_fn)
+print(y.grad_fn) # 연산의 결과는 grad_fn을 가지게 됨
 
 z = y * y * 3
 print("z=", z)
@@ -19,7 +24,7 @@ print("out=z.mean()=", out)
 
 # backprop
 out.backward()
-print("x.grad=", x.grad)
+print("x.grad=", x.grad) # 변화도
 
 x = torch.randn(3)
 y = Variable(x, requires_grad=True)
